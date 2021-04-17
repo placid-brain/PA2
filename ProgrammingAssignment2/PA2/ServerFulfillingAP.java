2ed0aeef8b05463945df8dc49f5c99fd62104b27
Hannah Theint Myat Wai <54528127+hannahmyatwai@users.noreply.github.com>
2021-04-18 00:44:24 +0800

    some update

--- ProgrammingAssignment2/PA2/ServerFulfillingAP.java
+++ ProgrammingAssignment2/PA2/ServerFulfillingAP.java
@@ -5,14 +5,20 @@ import java.io.FileOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.Key;
-//import java.security.cert.X509Certificate;
+import java.security.cert.X509Certificate;
 import java.security.PrivateKey;
+import java.securtiy.Key.getEncoded;
+import java.util.Base64;
+
 public class ServerFulfillingAP {
 
 	public static void main(String[] args) {
 		final X509Certificate x509certificate = CertificateReader.get("/home/myat00/Desktop/PA2/ProgrammingAssignment2/PA2/certificate_1004637.crt");
 		//get private key since we need private key to sign a message and send that message to the client.
 		final PrivateKey privateKey = PrivateKeyReader.get("/home/myat00/Desktop/PA2/ProgrammingAssignment2/PA2/private_key.der");
+		
+		//create an RSAUtils object to be able to use encrypt and decrypt methods
+		RSAUtils rsa = new RSAUtils();
 		
 		
 		int port = 4321;
@@ -45,10 +51,20 @@ public class ServerFulfillingAP {
 					//M = Hello this is Secstore using Ks- (private key)
 					// Hello, this is SecStore should be encrypted with private key
 					//encryption using private key
-					byte[] encryptedBytesArray = RSA.encrypt(fromClient.readUTF().getBytes(), privateKey);
+					byte[] encryptedBytesArray = rsa.encrypt(fromClient.readUTF().getBytes(), privateKey);
 					String encrypted_string_m = Base64.getEncoder().encodeToString(encryptedBytesArray);
 					//reply the encrypted string M to client
 					toClient.writeUTF(encrypted_string_m);
+					
+				}
+				
+				//requested for certification, certificate request = 3
+				if (packetType == 3){
+					//send the signed certificate to the client
+					//we can't pass in certificate crt format, hence, need to encode it into string, then, send to the client
+					String certif = x509Certificate.getEncoded();
+					certif = Base64.getEncoder().encodeToString(certif);
+					toClient.writeUTF(certif);
 					
 				}
 				
