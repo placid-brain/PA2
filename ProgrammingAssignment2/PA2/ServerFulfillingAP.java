dc2a978989410d7144683c12040512fa0fb89b73
placid_brain <shwetha_selvam@mymail.sutd.edu.sg>
2021-04-20 12:01:45 +0800

    update ver of Server AP

--- ProgrammingAssignment2/PA2/ServerFulfillingAP.java
+++ ProgrammingAssignment2/PA2/ServerFulfillingAP.java
@@ -1,31 +1,52 @@
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.Key;
+import java.security.PublicKey;
+import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.PrivateKey;
-import java.securtiy.Key.getEncoded;
 import java.util.Base64;
 
 public class ServerFulfillingAP {
 
 	public static void main(String[] args) {
-		final X509Certificate x509certificate = CertificateReader.get("/home/myat00/Desktop/PA2/ProgrammingAssignment2/PA2/certificate_1004637.crt");
+		InputStream fis = null;
+		try {
+			fis = new FileInputStream("/home/myat00/Desktop/PA2/ProgrammingAssignment2/PA2/certificate_1004637.crt");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		CertificateFactory cf = null;
+		try {
+			cf = CertificateFactory.getInstance("X.509");
+		} catch (CertificateException e) {
+			e.printStackTrace();
+		}
+		X509Certificate x509Certificate = null;
+		try {
+			x509Certificate = (X509Certificate) cf.generateCertificate(fis);
+		} catch (CertificateException e) {
+			e.printStackTrace();
+		}
 		//get private key since we need private key to sign a message and send that message to the client.
-		final PrivateKey privateKey = PrivateKeyReader.get("/home/myat00/Desktop/PA2/ProgrammingAssignment2/PA2/private_key.der");
-		
+		PrivateKey privateKey = null;
+		try {
+			privateKey = PrivateKeyReader.get("/home/myat00/Desktop/PA2/ProgrammingAssignment2/PA2/private_key.der");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 		//create an RSAUtils object to be able to use encrypt and decrypt methods
 		RSAUtils rsa = new RSAUtils();
-		
-		
+
+
 		int port = 4321;
-	    	if (args.length > 0) {
-	    		port = Integer.parseInt(args[0]);
-	    	}
-	    	
+		if (args.length > 0) {
+			port = Integer.parseInt(args[0]);
+		}
+
 		ServerSocket welcomeSocket = null;
 		Socket connectionSocket = null;
 		DataOutputStream toClient = null;
@@ -38,36 +59,37 @@ public class ServerFulfillingAP {
 			connectionSocket = welcomeSocket.accept();
 			fromClient = new DataInputStream(connectionSocket.getInputStream());
 			toClient = new DataOutputStream(connectionSocket.getOutputStream());
-			
+
 			while (!connectionSocket.isClosed()) {
 
 				int packetType = fromClient.readInt();
-				//	TODO: 	Print the packetType 
-				System.out.println(packetType);
-				
-				
+				//	TODO: 	Print the packetType
+				System.out.println("Packet type: "+packetType);
+
+
 				//If the packet is proving the identity of server
 				if (packetType == 2){
 					//M = Hello this is Secstore using Ks- (private key)
 					// Hello, this is SecStore should be encrypted with private key
 					//encryption using private key
-					byte[] encryptedBytesArray = rsa.encrypt(fromClient.readUTF().getBytes(), privateKey);
+					byte[] encryptedBytesArray = rsa.encrypt(fromClient.readUTF(), privateKey);
+					//encrypted message
 					String encrypted_string_m = Base64.getEncoder().encodeToString(encryptedBytesArray);
 					//reply the encrypted string M to client
 					toClient.writeUTF(encrypted_string_m);
-					
+
 				}
-				
+
 				//requested for certification, certificate request = 3
 				if (packetType == 3){
 					//send the signed certificate to the client
 					//we can't pass in certificate crt format, hence, need to encode it into string, then, send to the client
-					String certif = x509Certificate.getEncoded();
-					certif = Base64.getEncoder().encodeToString(certif);
+					byte[] certifByte = x509Certificate.getEncoded();
+					String certif = Base64.getEncoder().encodeToString(certifByte);
 					toClient.writeUTF(certif);
-					
+
 				}
-				
+
 				// If the packet is for transferring the filename
 				if (packetType == 0) {
 
@@ -75,13 +97,13 @@ public class ServerFulfillingAP {
 
 					int numBytes = fromClient.readInt();
 					byte [] filename = new byte[numBytes];
-				
+
 					fromClient.readFully(filename, 0, numBytes);
 
 					fileOutputStream = new FileOutputStream("recv_"+new String(filename, 0, numBytes));
 					bufferedFileOutputStream = new BufferedOutputStream(fileOutputStream);
-					
-				// If the packet is for transferring a chunk of the file
+
+					// If the packet is for transferring a chunk of the file
 				} else if (packetType == 1) {
 
 					int numBytes = fromClient.readInt();
@@ -101,13 +123,12 @@ public class ServerFulfillingAP {
 						connectionSocket.close();
 					}
 				}
-				//	TODO:	Need to fix this for Authentication protocol
-				
+
 			}
 		} catch (Exception e) {e.printStackTrace();}
 
-		
-	
-	
+
+
+
 	}
 }
